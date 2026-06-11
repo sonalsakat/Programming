db8f887af2a0901223c1d695323590d23c6b34a2
sonalsakat <157041264+sonalsakat@users.noreply.github.com>
2026-06-11 20:13:18 +0530

    Add files via upload

---
+++ C_programming/Practice/program71.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+    int iCnt = 0;
+
+    iCnt = 1;
+
+    while(iCnt <= 5)
+    {
+        printf("Jay Ganesh...\n");
+        iCnt++;
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ C_programming/Practice/program74.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    
+    int iCnt = 5;
+
+    while(iCnt >= 1)
+    {
+        printf("%d\t",iCnt);
+        iCnt--;
+    }
+
+    printf("\n");
+
+    return 0;
+}
